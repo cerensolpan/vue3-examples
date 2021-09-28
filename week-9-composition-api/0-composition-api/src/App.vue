8df3c37e15d3bb7db0a5d9fd4e3910be5e3295af
Ceren ŞOLPAN <73752127+cerensolpan@users.noreply.github.com>
2021-09-29 00:34:18 +0300

    Create Vite

---
+++ week-9-composition-api/0-composition-api/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>{{title}}</h3>
+  <input type="text" v-model="title"/>
+</template>
+<script>
+import {ref} from "vue";
+export default {
+  // data(){
+  //   return{
+  //     title: "Bu bir başlık"
+  //   }
+  // }
+  setup() {
+    const title = ref("Bu bir Setup Başlık");
+
+    return {
+      title
+    }
+  }
+}
+</script>
