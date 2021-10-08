1edab01c2ac5b0c911754ffcb28bdf19cd4d4a42
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2021-10-09 02:23:06 +0300

    Create script setup

---
+++ week-10-composition-api/0-script-setup/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>{{title}}</h3>
+  <input type="text" v-model="title">
+  <button @click="inc">{{counter}}</button>
+  <hr>
+  <oddOrEven :counter="counter"/>
+</template>
+<script setup>
+import oddOrEven from "./components/oddOrEven.vue"
+import {ref} from "vue"
+const title=ref("Başlık");
+const counter = ref(0);
+const inc = () => {
+  counter.value++
+}
+</script>
---
+++ week-10-composition-api/0-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>aa</h1>
+  <h3>{{result}}</h3>
+</template>
+<script setup>
+import {computed} from "vue";
+const props = defineProps({counter:Number});
+const result = computed(()=>(props.counter%2===0? 'Çift':'Tek'))
+</script>
