a8f4e17ba15f145825af87cbb93cfa5c26a2e5da
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2021-10-17 00:45:35 +0300

    Create suspense

---
+++ week-11/suspense-example/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {onErrorCaptured, ref, defineAsyncComponent} from "@vue/runtime-core"
+// import Todos from "./components/Todos.vue"
+// import User from "./components/User.vue"
+const Todos =defineAsyncComponent(()=> import("./components/Todos.vue"))
+const User =defineAsyncComponent(()=> import("./components/User.vue"))
+onErrorCaptured((e)=>{
+  err.value=e;
+  return true;
+})
+</script>
+
+<template>
+<span v-if="err" class="error">{{err}}</span>
+  <suspense v-else>
+      <template #default>
+        <div>
+          <User/>
+          <hr>
+          <Todos/>
+        </div>
+      </template>
+      <template #fallback>
+        <div>Loading..</div>
+      </template>
+  </suspense>
+  <!-- <User/> -->
+</template>
+
+
