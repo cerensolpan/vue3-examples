d6ee8a50f1b7735bc58742b92b30eefbe1140fb6
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2021-10-22 01:09:33 +0300

    Change to script setup

--- week-12-socialmark-composition-api/client/src/views/Home.vue
+++ week-12-socialmark-composition-api/client/src/views/Home.vue
@@ -6,48 +6,43 @@
     <div v-else>Bookmark bulunmamaktadır..</div>
   </div>
 </template>
-<script>
+<script setup>
 import Sidebar from "@/components/Home/Sidebar";
-export default {
-  components: {
-    Sidebar
-  },
-  data() {
-    return {
-      bookmarkList: []
-    };
-  },
-  mounted() {
-    this.$socket.on("NEW_BOOKMARK_ADDED", bookmark => {
-      this.bookmarkList.push(bookmark);
-    });
-  },
-  created() {
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      this.$appAxios.get("/bookmarks?_expand=category&_expand=user").then(bookmark_list_response => {
-        console.log("bookmark_list_response :>> ", bookmark_list_response);
-        this.bookmarkList = bookmark_list_response?.data || [];
-      });
-      //! Bookmark olarak eklediklerimizi çekmek için user_bookmarks üzerinden çekelim..
-      this.$appAxios.get("/user_bookmarks/?_expand=bookmark&_expand=user").then(user_bookmark_response => {
-        console.log("user_bookmark_response :>> ", user_bookmark_response);
-        this.$store.commit("setBookmarks", user_bookmark_response?.data);
-      });
+import {ref,onMounted,inject} from 'vue';
+import {useStore} from "vuex";
+const appAxios = inject("appAxios");
+const socket = inject("socket");
+const bookmarkList= ref([]);
+const store = useStore();
 
-      //! Like olarak eklediklerimizi çekmek için user_likes üzerinden çekelim..
-      this.$appAxios.get("/user_likes/?_expand=bookmark&_expand=user").then(user_likes_response => {
-        this.$store.commit("setLikes", user_likes_response?.data);
-      });
-    },
-    updateBookmarkList(categoryId) {
-      const url = categoryId ? `/bookmarks?_expand=category&_expand=user&categoryId=${categoryId}` : `/bookmarks?_expand=category&_expand=user`;
-      this.$appAxios.get(url).then(bookmark_list_response => {
-        this.bookmarkList = bookmark_list_response?.data || [];
-      });
-    }
-  }
-};
+onMounted(()=>{
+      socket.on("NEW_BOOKMARK_ADDED", bookmark => {
+      bookmarkList.value.push(bookmark);
+    });
+})
+
+const fetchData = () => {
+  appAxios.get("/bookmarks?_expand=category&_expand=user").then(bookmark_list_response => {
+    bookmarkList.value = bookmark_list_response?.data || [];
+  });
+  //! Bookmark olarak eklediklerimizi çekmek için user_bookmarks üzerinden çekelim..
+  appAxios.get("/user_bookmarks/?_expand=bookmark&_expand=user").then(user_bookmark_response => {
+    store.commit("setBookmarks", user_bookmark_response?.data);
+  });
+
+  //! Like olarak eklediklerimizi çekmek için user_likes üzerinden çekelim..
+  appAxios.get("/user_likes/?_expand=bookmark&_expand=user").then(user_likes_response => {
+    store.commit("setLikes", user_likes_response?.data);
+  });
+}
+
+const updateBookmarkList = (categoryId) => {
+  const url = categoryId ? `/bookmarks?_expand=category&_expand=user&categoryId=${categoryId}` : `/bookmarks?_expand=category&_expand=user`;
+  appAxios.get(url).then(bookmark_list_response => {
+    bookmarkList.value = bookmark_list_response?.data || [];
+  });
+}
+
+fetchData();
+
 </script>
